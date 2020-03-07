44d264508f0389e92b61888bf7292a57a0cd3e00
Mathew Ramirez <Mathew.J.Ramirez@gmail.com>
2020-03-07 11:01:15 -0600

    Changed some background images in sky

--- react-app/src/pages/LoginPage.js
+++ react-app/src/pages/LoginPage.js
@@ -4,24 +4,32 @@ import Login from "./../components/Login";
 import Sky from "react-sky";
 
 class LoginPage extends Component {
+
+  //function here
+
+
   render() {
     return (
+
+      //function can be called
+      
       <div>
         
         <Sky
           images={{
             /* FORMAT AS FOLLOWS */
-            0: "https://cdn.pixabay.com/photo/2016/03/31/20/40/baby-1295959_960_720.png" /* You can pass as many images as you want */,
+            0: "https://media3.giphy.com/media/fo2dhRTmaULbStoFkX/giphy.webp?cid=790b7611941c38216e03aa35de213c65f959a1ce9d7bf3fa&rid=giphy.webp" /* You can pass as many images as you want */,
             1: "https://cdn.discordapp.com/attachments/628743072334872630/685276679303987254/Flower336.png",
             2: "https://cdn.discordapp.com/attachments/628743072334872630/685276641139621896/Cherry336.gif",
             3: "https://www.pngkit.com/png/full/661-6613192_freetoedit-cute-kawaii-pixel-pastel-rose-jfashion-png.png",
             4: "https://cdn.discordapp.com/attachments/628743072334872630/685276601503842324/Heart336.gif",
-            5: "https://cdn.discordapp.com/attachments/628743072334872630/685276641139621896/Cherry336.gif"
+            5: "https://cdn.discordapp.com/attachments/628743072334872630/685276641139621896/Cherry336.gif",
+            6: "https://media3.giphy.com/media/fo2dhRTmaULbStoFkX/giphy.webp?cid=790b7611941c38216e03aa35de213c65f959a1ce9d7bf3fa&rid=giphy.webp"
           }}
           how={
-            160
+            250
           } /* Pass the number of images Sky will render chosing randomly */
-          time={300} /* time of animation */
+          time={15} /* time of animation */
           size={"100px"} /* size of the rendered images */
           background={"palettedvioletred"} /* color of background */
         />
