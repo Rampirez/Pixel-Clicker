f9ccf203ab91488bc0d14c32194172bbf6adc709
Mathew Ramirez <mathew.j.ramirez@gmail.com>
2020-03-17 19:47:05 -0500

    Added heroku and mongoose

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://localhost:27017/mern-auth",
-    secretOrKey: "secret"
+    mongoURI: process.env.MONGODB_URI,
+    secretOrKey: process.env.SECRET_OR_KEY
 };
