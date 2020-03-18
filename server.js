86c21b7f5d068ee5f68b6117150ed764c36ae70d
Mathew Ramirez <mathew.j.ramirez@gmail.com>
2020-03-17 19:36:35 -0500

    adding heroku capability

--- server.js
+++ server.js
@@ -33,5 +33,14 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
